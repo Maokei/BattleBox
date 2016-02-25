bc2f6f4c15c5ca70a685524f5460759d3c02ab9e
Maokei <rickard.joh@gmail.com>
2016-02-25 01:43:36 +0100

    inital webserver commit, snooze time

--- server/app.js
+++ server/app.js
@@ -2,3 +2,20 @@
  * @author maokei
  * @description Game server
  * */
+
+
+var http = require('http');
+var path = require('path');
+var port = 7777;
+
+//serv up client
+var server = http.createServer(function(req, res) {
+    console.log('we got request');
+    res.writeHead(200, {'Content-type':'text/html'});
+    res.write("<p>Battlebox test</p>");
+    res.end();
+});
+
+server.listen(port, function() {
+    console.log("BattleBox server is live at port: " + port);
+});
