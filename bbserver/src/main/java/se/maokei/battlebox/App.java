920040694f2240b8d70c3a377382e5b10d0a5e37
maokei <rickard.joh@gmail.com>
2018-01-07 19:50:52 +0100

    Bare bones erver

--- bbserver/src/main/java/se/maokei/battlebox/App.java
+++ bbserver/src/main/java/se/maokei/battlebox/App.java
@@ -1,13 +1,29 @@
+
 package se.maokei.battlebox;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+
 /**
- * Hello world!
+ * 
  *
  */
-public class App 
+public class App extends AbstractVerticle 
 {
-    public static void main( String[] args )
+    /*public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
+    }*/
+    
+    @Override
+	public void start() {
+		System.out.println("Starting server");
+		vertx.createHttpServer().requestHandler(req -> {
+        	System.out.println("Starting server");
+              req.response()
+                .putHeader("content-type", "text/plain")
+                .end("Hello from Vert.x!");
+            }).listen(8080);
+        System.out.println("HTTP server started on port 8080");
+	}
 }
